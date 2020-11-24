cd3349de2d0ed08f23a537c29f59d358d8c68688
MikeyPsyche <mikepartfour@icloud.com>
2020-11-23 23:11:50 -0500

    FE Boiler

--- script.py
+++ script.py
@@ -1,4 +1,22 @@
 '''
 This is snoomarks 2.0
 
+A will get faster trust me, I am rusty with python, don't judge me, I know I'm prolly getting clowned
+
 '''
+
+import praw 
+
+
+'''
+
+CLIENT_ID = 
+
+CLIENT_SECRET = 
+
+REDIRECT_URI = "https://127.0.0.1:65010/authorize_callback"
+
+r = praw.Reddit(CLIENT_ID= , CLIENT_SECRET= , password= , user_agent= , username= )
+
+'''
+# app = 
